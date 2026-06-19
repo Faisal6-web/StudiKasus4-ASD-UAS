8da7042c2aa7f07d8b6bca9ee80af0692650b86e
rakhamadani <rakhamadani789@gmail.com>
2026-06-19 18:13:06 +0700

    Rakha : Fungsi Traversal

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -101,14 +101,26 @@ public class BinarySearchTree {
 
     // Rakha: Fungsi Traversal
     void preOrder(Node root) {
-
+        if (root != null) {
+            System.out.print(root.data + " ");
+            preOrder(root.left);
+            preOrder(root.right);
+        }
     }
 
     void inOrder(Node root) {
-
+        if (root != null) {
+            inOrder(root.left);
+            System.out.print(root.data + " ");
+            inOrder(root.right);
+        }
     }
 
     void postOrder(Node root) {
-
+        if (root != null) {
+            postOrder(root.left);
+            postOrder(root.right);
+            System.out.print(root.data + " ");
+        }
     }
 }
